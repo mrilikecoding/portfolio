2807b3465bcc2f6bc7b9588acc4842157e75f4fc
mrilikecoding <nathanrgreen@gmail.com>
2014-01-18 15:09:29 -0600

    disable mongo for now

--- app.rb
+++ app.rb
@@ -15,28 +15,12 @@ include Mongo
 configure do
   set :views, "#{File.dirname(__FILE__)}/views"
 
-  #MongoDB
-  conn = MongoClient.new("localhost", 27017)
-  set :mongo_connection, conn
-  set :mongo_db, conn.db('nathanrgreen')
+  ##MongoDB
+  #conn = MongoClient.new("localhost", 27017)
+  #set :mongo_connection, conn
+  #set :mongo_db, conn.db('nathanrgreen')
 end
 
-
-assets do
-
-  js :application, %w(
-    /js/*.js
-    /js/vendor/*.js
-  )
-  css :application, %w(
-    /css/jqueryui.css
-    /css/reset.css
-  )
-
-  js_compression :jsmin
-  css_compression :sass
-
-end
 
 #for local vars
 app_config = {
@@ -75,59 +59,59 @@ end
 
 ##API##
 
-
-# show all collections
-get '/collections/?' do
-  settings.mongo_db.collection_names
-end
-
-# list all documents in the test collection
-get '/documents/?' do
-  content_type :json
-  settings.mongo_db['nathanrgreen'].find.to_a.to_json
-end
-
-# find a document by its ID
-get '/document/:id/?' do
-  content_type :json
-  document_by_id(params[:id]).to_json
-end
-
-# insert a new document from the request parameters,
-# then return the full document
-post '/new_document/?' do
-  content_type :json
-  new_id = settings.mongo_db['nathanrgreen'].insert params
-  document_by_id(new_id).to_json
-end
-
-# update the document specified by :id, setting its
-# contents to params, then return the full document
-put '/update/:id/?' do
-  content_type :json
-  id = object_id(params[:id])
-  #settings.mongo_db['nathanrgreen'].update(:_id => id, params)
-  #document_by_id(id).to_json
-end
-
-# update the document specified by :id, setting just its
-# name attribute to params[:name], then return the full
-# document
-put '/update_name/:id/?' do
-  content_type :json
-  id   = object_id(params[:id])
-  name = params[:name]
-  settings.mongo_db['nathanrgreen'].
-      #update(:_id => id, {"$set" => {:name => name}})
-  document_by_id(id).to_json
-end
-
-# delete the specified document and return success
-delete '/remove/:id' do
-  content_type :json
-  settings.mongo_db['nathanrgreen'].
-      remove(:_id => object_id(params[:id]))
-  {:success => true}.to_json
-end
+#
+## show all collections
+#get '/collections/?' do
+#  settings.mongo_db.collection_names
+#end
+#
+## list all documents in the test collection
+#get '/documents/?' do
+#  content_type :json
+#  settings.mongo_db['nathanrgreen'].find.to_a.to_json
+#end
+#
+## find a document by its ID
+#get '/document/:id/?' do
+#  content_type :json
+#  document_by_id(params[:id]).to_json
+#end
+#
+## insert a new document from the request parameters,
+## then return the full document
+#post '/new_document/?' do
+#  content_type :json
+#  new_id = settings.mongo_db['nathanrgreen'].insert params
+#  document_by_id(new_id).to_json
+#end
+#
+## update the document specified by :id, setting its
+## contents to params, then return the full document
+#put '/update/:id/?' do
+#  content_type :json
+#  id = object_id(params[:id])
+#  #settings.mongo_db['nathanrgreen'].update(:_id => id, params)
+#  #document_by_id(id).to_json
+#end
+#
+## update the document specified by :id, setting just its
+## name attribute to params[:name], then return the full
+## document
+#put '/update_name/:id/?' do
+#  content_type :json
+#  id   = object_id(params[:id])
+#  name = params[:name]
+#  settings.mongo_db['nathanrgreen'].
+#      #update(:_id => id, {"$set" => {:name => name}})
+#  document_by_id(id).to_json
+#end
+#
+## delete the specified document and return success
+#delete '/remove/:id' do
+#  content_type :json
+#  settings.mongo_db['nathanrgreen'].
+#      remove(:_id => object_id(params[:id]))
+#  {:success => true}.to_json
+#end
 
 
